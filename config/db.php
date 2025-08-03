b134017eca8061fccfcc0d8f136bf2e1b33f7dc9
wakeupparal1zed <wakeupparal1zed.cpp@gmail.com>
2025-08-04 02:31:10 +0300

    Update db.php gres

--- config/db.php
+++ config/db.php
@@ -1,33 +1,25 @@
 <?php
-$dbname   = 'taskich';
-$user     = '123';
-$password = 'verysecurepasswordgagaga';
-$charset  = 'utf8mb4';
 
-$dsn = "mysql:host=mysql;dbname=$dbname;charset=$charset";
-try {#чтоб трейсбек не вывалило кучей в браузер
-    $pdo = new PDO($dsn, $user, $password, [
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_BOTH,  
-        PDO::ATTR_EMULATE_PREPARES   => false,            
-    ]);
+$host = getenv('DB_HOST') ?: 'db';                 
+$name = getenv('DB_NAME') ?: 'taskich';
+$user = getenv('DB_USER') ?: 'pguser';
+$pass = getenv('POSTGRES_PASSWORD') ?: 'verysecurepasswordgagaga';
+
+$dsn  = "pgsql:host={$host};dbname={$name};options='--client_encoding=UTF8'";
+
+try {
+    $pdo = new PDO(
+        $dsn,
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ]
+    );
 } catch (PDOException $e) {
-    exit('DB connect failed');
+    http_response_code(500);
+    exit("DB connect error: " . $e->getMessage());
 }
-
-$pdo->exec("CREATE TABLE IF NOT EXISTS notsecret (
-    id INT PRIMARY KEY,
-    qq VARCHAR(255)
-)");
-
-$pdo->exec("CREATE TABLE IF NOT EXISTS secret (
-    id INT PRIMARY KEY, 
-    qq VARCHAR(255) NOT NULL
-)");
-
-$check = $pdo->query("SELECT COUNT(*) FROM notsecret")->fetchColumn();#fix later уже забыл че тут вообще написано
-if ($check == 0) {
-    $pdo->exec("INSERT INTO notsecret (id, qq) VALUES
-        (1, 'notsecret1'), (2, 'notsecret2'), (3, 'notsecret3'),(4, 'notsecret4'), (5, 'notsecret5'), (6, 'notsecret6'),(7, 'notsecret7'), (8, 'notsecret8'), (9, 'notsecret9'),(10, 'notsecret10')");
-        
-    $pdo->exec("INSERT INTO secret VALUES (1, 'flag{ihopeicangetaninternship}')");
-}
+?>
