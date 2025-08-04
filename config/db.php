8a46f62df0ff0c8723ac796f397d6371e461d4bb
wakeupparal1zed <wakeupparal1zed.cpp@gmail.com>
2025-08-04 03:54:11 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,17 +1,35 @@
 <?php
-
 $host = 'db';
 $port = '5432';
 $dbname = 'taskich';
 $user = 'pguser';
 $password = 'verysecurepasswordgagaga';
 
-$dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+$pdo = new PDO(
+    "pgsql:host=$host;port=$port;dbname=$dbname",
+    $user,
+    $password,
+    [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ]
+);
 
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+$pdo->exec("
+CREATE TABLE IF NOT EXISTS notsecret(
+    id SERIAL PRIMARY KEY,
+    qq TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS secret(
+    id SERIAL PRIMARY KEY,
+    qq TEXT NOT NULL
+);
+");
 
-return new PDO($dsn, $user, $password, $options);
+if ($pdo->query('SELECT COUNT(*) FROM notsecret')->fetchColumn() == 0) {
+    $ins = $pdo->prepare('INSERT INTO notsecret(qq) VALUES(:qq)');
+    for ($i = 1; $i <= 10; $i++) $ins->execute([':qq' => \"notsecret$i\"]);
+    $pdo->prepare('INSERT INTO secret(qq) VALUES(:qq)')
+        ->execute([':qq' => 'flag{ihopeicangetaninternship}']);
+}
