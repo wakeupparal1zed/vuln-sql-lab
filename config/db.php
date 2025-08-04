909eaac859f96a7ae75d028b873e15235773f8e3
wakeupparal1zed <wakeupparal1zed.cpp@gmail.com>
2025-08-04 03:13:51 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,25 +1,17 @@
 <?php
 
-$host = getenv('DB_HOST') ?: 'db';                 
-$name = getenv('DB_NAME') ?: 'taskich';
-$user = getenv('DB_USER') ?: 'pguser';
-$pass = getenv('POSTGRES_PASSWORD') ?: 'verysecurepasswordgagaga';
+$host = 'db';
+$port = '5432';
+$dbname = 'taskich';
+$user = 'pguser';
+$password = 'verysecurepasswordgagaga';
 
-$dsn  = "pgsql:host={$host};dbname={$name};options='--client_encoding=UTF8'";
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
 
-try {
-    $pdo = new PDO(
-        $dsn,
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ]
-    );
-} catch (PDOException $e) {
-    http_response_code(500);
-    exit("DB connect error: " . $e->getMessage());
-}
-?>
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+return new PDO($dsn, $user, $password, $options);
