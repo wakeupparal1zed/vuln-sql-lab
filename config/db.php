dac898817ddf5ff60a482d93c82f07ec39c1bf0c
wakeupparal1zed <wakeupparal1zed.cpp@gmail.com>
2025-08-04 04:08:16 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,35 +1,11 @@
 <?php
-$host = 'db';
-$port = '5432';
-$dbname = 'taskich';
-$user = 'pguser';
-$password = 'verysecurepasswordgagaga';
-
-$pdo = new PDO(
-    "pgsql:host=$host;port=$port;dbname=$dbname",
-    $user,
-    $password,
-    [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-    ]
-);
+$pdo=new PDO("pgsql:host=db;port=5432;dbname=taskich","pguser","verysecurepasswordgagaga",[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,PDO::ATTR_EMULATE_PREPARES=>false]);
 
 $pdo->exec("
-CREATE TABLE IF NOT EXISTS notsecret(
-    id SERIAL PRIMARY KEY,
-    qq TEXT NOT NULL
-);
-CREATE TABLE IF NOT EXISTS secret(
-    id SERIAL PRIMARY KEY,
-    qq TEXT NOT NULL
-);
+CREATE TABLE IF NOT EXISTS notsecret(id SERIAL PRIMARY KEY,qq text);
+CREATE TABLE IF NOT EXISTS secret(id SERIAL PRIMARY KEY,qq text);
 ");
-
-if ($pdo->query('SELECT COUNT(*) FROM notsecret')->fetchColumn() == 0) {
-    $ins = $pdo->prepare('INSERT INTO notsecret(qq) VALUES(:qq)');
-    for ($i = 1; $i <= 10; $i++) $ins->execute([':qq' => \"notsecret$i\"]);
-    $pdo->prepare('INSERT INTO secret(qq) VALUES(:qq)')
-        ->execute([':qq' => 'flag{ihopeicangetaninternship}']);
+if(!$pdo->query('SELECT 1 FROM notsecret LIMIT 1')->fetch()){
+    for($i=1;$i<=10;$i++)$pdo->exec("INSERT INTO notsecret(qq) VALUES('notsecret{$i}')");
+    $pdo->exec(\"INSERT INTO secret(qq) VALUES('flag{ihopeicangetaninternship}')\");
 }
